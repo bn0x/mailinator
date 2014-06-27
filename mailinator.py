3a30759d0bb4540cf5f580ca19cff5aba4ffbc38
obnoxious <obnoxious@dongcorp.org>
2014-06-27 00:31:48 -0700

    i hate life

---
+++ mailinator.py
@@ -0,0 +1,24 @@
+import requests
+
+class instance(object):
+    def __init__(self, api_key="", *args, **kwargs):
+        self.session = requests.session()
+        self.endpoint = []
+        self.apiKey = api_key
+
+    def __getattr__(self, thing):
+        self.endpoint.append(thing)
+        return self
+
+    def __call__(self, *args, **kwargs):
+        endpoint = "/".join(["https://api.mailinator.com/api"] + self.endpoint)
+        self.endpoint = []
+        return self.request(endpoint, *args, **kwargs)
+
+    def request(self, endpoint, *args, **kwargs):
+        kwargs.setdefault("token", self.apiKey)
+        request = self.session.get(endpoint, params=kwargs)
+        return request.json()
+
+if __name__ == "__main__":
+    print("You don't run this..")
---
+++ test.py
@@ -0,0 +1,4 @@
+import mailinator
+
+imgay = mailinator.instance(api_key="YOUR API KEY")
+print(imgay.inbox(to="spam"))
