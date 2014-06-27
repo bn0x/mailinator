d4c5c4c81f70e43daae8398a7f0b0172cdc56e2f
obnoxious <obnoxious@dongcorp.org>
2014-06-27 00:39:05 -0700

    setup.py ;)))

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='mailinator',
+      version='1.0',
+      description='mailinator python library',
+      author='obnoxious',
+      author_email='obnoxious@dongcorp.org',
+      url='http://github.com/bn0x/mailinator',
+      packages=['mailinator'],
+     )
